15f1b47c8c800f21e7b534810b62a75cd87f1151
Josh <josh@invaware.com>
2017-09-15 14:05:51 -0400

    chart widget for each view to display previous 7 x

--- money/views.py
+++ money/views.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import uuid
+
+from datetime import timedelta
+
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
+from django.utils import timezone
+from django.utils.translation import ugettext_lazy as _
 
 from .models import DailyLedger, FixedAmount, Transaction
 from .forms import TransactionForm
+from .utils import week_range
 
 
 class FixedAmountsView(ListView):
@@ -30,7 +37,11 @@ class TransactionsListBaseView(ListView):
     def get_context_data(self, **kwargs):
         ctx = super(TransactionsListBaseView, self).get_context_data(**kwargs)
         ctx['ledger'] = self.get_transaction_data()
+        ctx['id'] = str.replace(str(uuid.uuid4()), '-', '')
         return ctx
+
+    def parse_ledger_data(self, ledgers):
+        pass
 
 
 class ThisMonthsTransactionsListView(TransactionsListBaseView):
@@ -39,6 +50,11 @@ class ThisMonthsTransactionsListView(TransactionsListBaseView):
     def get_queryset(self):
         return DailyLedger.objects.this_month(self.request.user)
 
+    def get_context_data(self, **kwargs):
+        ctx = super(ThisMonthsTransactionsListView, self).get_context_data(**kwargs)
+        ctx['chart_label'] = _('Previous 7 Months')
+        return ctx
+
 
 class ThisWeeksTransactionsListView(TransactionsListBaseView):
     template_name = 'money/transactions.html'
@@ -46,12 +62,32 @@ class ThisWeeksTransactionsListView(TransactionsListBaseView):
     def get_queryset(self):
         return DailyLedger.objects.this_week(self.request.user)
 
+    def get_context_data(self, **kwargs):
+        ctx = super(ThisWeeksTransactionsListView, self).get_context_data(**kwargs)
+        ctx['chart_label'] = _('Previous 7 Weeks')
+        return ctx
+
 
 class TodaysTransactionsListView(TransactionsListBaseView):
     template_name = 'money/transactions.html'
 
     def get_queryset(self):
         return [DailyLedger.objects.today()]
+
+    def get_context_data(self, **kwargs):
+        ctx = super(TodaysTransactionsListView, self).get_context_data(**kwargs)
+        now = timezone.now()
+        start, end = week_range(now)
+        delta = timedelta(days=7 - (now.weekday() + 1))
+        start = start - delta
+        end = end - delta
+        ledgers = (DailyLedger
+                   .objects
+                   .filter(created_on__range=(start, end), owner=self.request.user)
+                   .order_by('created_on'))
+        ctx['ledgers'] = ledgers
+        ctx['chart_label'] = _('Previous 7 Days')
+        return ctx
 
 
 class TransactionCreateView(CreateView):
